7dbb4f37c11b18d0210ad392911ee313a133e034
N4nid <136599840+N4nid@users.noreply.github.com>
2024-03-20 15:18:32 +0100

    update

--- init.lua
+++ init.lua
@@ -2,3 +2,16 @@
 require("config.lazy")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-t>", ":Switch<CR>")
+-- add own cmd
+vim.g.colorscheme = "tokyonight"
+vim.api.nvim_create_user_command("Nobg", "hi Normal guibg=NONE ctermbg=NONE", {})
+vim.api.nvim_create_user_command("Bg", "colorscheme " .. vim.g.colorscheme, {})
+
+--theme stuff
+vim.cmd("Bg")
+--vim.cmd("Nobg")
+
+-- onStart cmds
+--require("ibl")
+--vim.cmd("IBLDisablew)
--- lua/plugins/easynums.lua
+++ lua/plugins/easynums.lua
@@ -1,10 +1,9 @@
 return {
-  {
-    "N4nid/easynums.nvim",
-    opts = {
-      num = "basdfghjkl", -- the keys to remap to 0123456789, b would be in this case 0
-      cancelKey = "c",
-      key = "ö", -- the activation key. Currently no support for combinations like <C-n>
-    },
+  dir = "~/Documents/GitHub/easynums.nvim",
+
+  opts = {
+    num = "basdfghjkl", -- the keys to remap to 0123456789, b would be in this case 0
+    cancelKey = "c",
+    key = "ö", -- the activation key. Currently no support for combinations like <C-n>
   },
 }
