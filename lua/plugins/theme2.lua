cf73c17a9df6c6214c4d87dd390a92378d84301d
N4nid <136599840+N4nid@users.noreply.github.com>
2024-03-30 21:02:27 +0100

    updated

---
+++ lua/plugins/theme2.lua
@@ -0,0 +1,39 @@
+return {
+  { "Everblush/nvim", as = "everblush" },
+  { "fenetikm/falcon", as = "falcon" },
+  {
+    "xero/miasma.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme miasma")
+    end,
+  },
+  --{ "ray-x/starry.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "Abstract-IDE/Abstract-cs" },
+  {
+    "lervag/vimtex",
+  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "baliestri/aura-theme",
+    lazy = false,
+    priority = 1000,
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+    end,
+  },
+  {
+    "nyngwang/nvimgelion",
+  },
+  {
+    "Yazeed1s/minimal.nvim",
+  },
+  { "glepnir/zephyr-nvim" },
+  { "ribru17/bamboo.nvim" },
+  { "sainnhe/everforest" },
+  { "lukas-reineke/headlines.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "sainnhe/sonokai" },
+}
