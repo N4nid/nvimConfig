87f00dbb35bfaee95f1d0aa35f4e71cf8634594a
N4nid <136599840+N4nid@users.noreply.github.com>
2024-05-12 14:32:01 +0200

    Updated config

--- init.lua
+++ init.lua
@@ -4,6 +4,13 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<Leader>m", ":MarkdownPreview<CR>")
 vim.keymap.set("n", "<C-t>", ":Switch<CR>")
+
+local lazyterm = function() LazyVim.terminal(nil, { cwd = LazyVim.root() }) end
+vim.keymap.set("n","<c-E>",lazyterm);
+vim.keymap.set("t","<c-O>","ranger<CR>");
+vim.keymap.set("t", "<C-e>", "<cmd>close<cr>", { desc = "Hide Terminal" });
+
+
 -- add own cmd
 
 vim.g.colorscheme = "tokyonight"
